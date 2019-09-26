e42a0b923f0eea7e644c01acaa29e1075f6b5166
Daniel Benítez BIGDATA <danielbenitezbigdata@Daniel-Benitez-Laptop.local>
2019-09-26 17:27:34 +0200

    3.1.2.15 lab

---
+++ collatz.py
@@ -0,0 +1,15 @@
+num=int(input("Enter your number: "))
+steps=0
+
+while num!=1:
+    steps+=1
+    if num%2==0:
+        #even
+        num=num//2
+    else:
+        #odd
+        num=3*num+1
+    print(num)
+
+print("steps",str(steps),sep=" = ")
+    
