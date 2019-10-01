a7324cb8fd37995c1d229e467381add1c36336ff
Neiklot <dbenitez83@live.com>
2019-10-01 18:16:23 +0200

    LeadYears

--- LeadYears.py
+++ LeadYears.py
@@ -1,8 +1,5 @@
 def isYearLeap(year):
-    if year%100==0:
-        if year%400==0:
-            return True
-    elif year%4==0:
+    if (year%100==0 and year%400==0) or (year%100!=0 and year%4==0):
         return True
     return False
 
