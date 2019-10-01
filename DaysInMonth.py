87b30661c05a23deb12f187e1fc5ae27a83ad400
Neiklot <dbenitez83@live.com>
2019-10-01 18:09:13 +0200

    daysInMonth

---
+++ DaysInMonth.py
@@ -0,0 +1,27 @@
+def isYearLeap(year):
+    if year%100==0:
+        if year%400==0:
+            return True
+    elif year%4==0:
+        return True
+    return False
+
+def daysInMonth(year, month):
+    daysInMonths=[None,31,28,31,30,31,30,31,31,30,31,30,31]
+    if (isYearLeap(year) and month==2):
+        return daysInMonths[month]+1
+    else:
+        return daysInMonths[month]
+
+testYears = [1900, 2000, 2016, 1987]
+testMonths = [2, 2, 1, 11]
+testResults = [28, 29, 31, 30]
+for i in range(len(testYears)):
+    yr = testYears[i]
+    mo = testMonths[i]
+    print(yr, mo, "->", end="")
+    result = daysInMonth(yr, mo)
+    if result == testResults[i]:
+        print("OK")
+    else:
+        print("Failed")
